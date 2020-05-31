6ce826b8bdfb64a093dc76d0f1a98474a911b1b2
Luan Mineu Costa <luanwebdev@gmail.com>
2020-05-31 17:31:34 -0300

    WebView simples finalizada

---
+++ main.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow, globalShortcut} = require('electron')
+const config =  require('./config')
+
+let win;
+
+function createWindow () {
+  // Cria uma janela de navegação.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    alwaysOnTop: true,
+    titleBarStyle: 'hidden',
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadURL(config.url)
+
+}
+
+function toggleDevTools(){
+    win.webContents.openDevTools()
+}
+
+function createShotcuts(){
+    globalShortcut.register('CmdOrCtrl+J', toggleDevTools);
+}
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+app.whenReady().then(createWindow).then(createShotcuts)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // No macOS é comum para aplicativos e sua barra de menu 
+  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
